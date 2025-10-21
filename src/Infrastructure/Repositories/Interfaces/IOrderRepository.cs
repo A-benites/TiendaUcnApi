b448be9a274daf1fe39fe12d048ac54c9a7ab1a7
Amir Benites <amir.benites@alumnos.ucn.cl>
2025-10-21 03:24:19 -0300

    feat: extend OrderRepository with query and update methods

--- src/Infrastructure/Repositories/Interfaces/IOrderRepository.cs
+++ src/Infrastructure/Repositories/Interfaces/IOrderRepository.cs
@@ -1,4 +1,5 @@
 
+using TiendaUcnApi.src.Application.DTO.OrderDTO;
 using TiendaUcnApi.src.Domain.Models;
 
 namespace TiendaUcnApi.src.Infrastructure.Repositories.Interfaces;
@@ -7,4 +8,7 @@ public interface IOrderRepository
 {
     Task<Order> CreateAsync(Order order);
     Task<IEnumerable<Order>> GetAllByUser(int userId);
+    Task<Order?> GetByIdAsync(int id);
+    Task<(IEnumerable<Order> Orders, int TotalCount)> GetAllAsync(OrderFilterDTO filter);
+    Task<Order> UpdateStatusAsync(int id, OrderStatus status);
 }
